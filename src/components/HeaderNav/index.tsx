cb08dfe3c36ab37e3fa6992867db7afb5ce4abb4
“算子” <1687712633@qq.com>
2022-12-27 20:02:04 +0800

    .

--- src/components/HeaderNav/index.tsx
+++ src/components/HeaderNav/index.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate, useNavigation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import './index.less'
 
 export const HeaderNav: React.FC = () => {
@@ -13,7 +13,8 @@ export const HeaderNav: React.FC = () => {
             <div className="nav-area">
                 <li onClick={() => router('/home')} className={`nav-item ${currentRouter === '/home' ? 'nav-active' : null}`}>首页</li>
                 <li onClick={() => router('/article')} className={`nav-item ${currentRouter === '/article' ? 'nav-active' : null}`}>文章</li>
-                <li onClick={() => router('/socureCode')} className={`nav-item ${currentRouter === '/socureCode' ? 'nav-active' : null}`}>源码</li>
+                <li onClick={() => router('/socureCode')} className={`nav-item ${currentRouter === '/socureCode' ? 'nav-active' : null}`}>源码解析</li>
+                <li onClick={() => router('/socureCode')} className={`nav-item ${currentRouter === '/util' ? 'nav-active' : null}`}>在线工具</li>
                 <li className="nav-item" >github</li>
             </div>
         </nav>
