e5296ac326e68b8f1eb0f7a3f2ca8a663cf6c788
“算子” <1687712633@qq.com>
2022-12-28 10:23:15 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { remarkCodeHike } from "@code-hike/mdx"
-//@ts-ignore
-import theme from "./public/github-dark.json"
+import { readFile } from 'fs/promises'
+
 export default defineConfig(async () => {
+    const theme = JSON.parse(await readFile(new URL('node_modules/shiki/themes/github-dark.json', import.meta.url)) as unknown as string)
     const mdx = await import("@mdx-js/rollup")
     return {
         plugins: [
